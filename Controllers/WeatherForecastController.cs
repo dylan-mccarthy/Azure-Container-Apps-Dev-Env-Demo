b00974793976089d08c066541c0edecc34f958a5
Dylan McCarthy <61097819+dylan-mccarthy@users.noreply.github.com>
2023-02-15 18:49:57 +1100

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Fine"
+        "Hot","Cold"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
